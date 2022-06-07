61667b40d1043126bff5ff0794d05ca68d181a97
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-07 07:38:34 -0300

    Criação da funcionalidade de buscar por IP

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate returns the command line application ready to run.
 func Generate() *cli.App {
@@ -8,5 +14,35 @@ func Generate() *cli.App {
 	app.Name = "Command Line App"
 	app.Usage = "Find ips and server names on the internet"
 	app.Version = "1.0.0"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Find ip addresses on internet",
+			Flags:  flags,
+			Action: findIps,
+		},
+	}
 	return app
+}
+
+func findIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println("IP:", ip)
+	}
 }
